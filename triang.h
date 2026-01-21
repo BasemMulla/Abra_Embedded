aa7d3a353926813ec34ab648f49f272855032b26
BasemMulla <142521578+BasemMulla@users.noreply.github.com>
2026-01-21 09:44:29 +0200

    Update triang.h

--- triang.h
+++ triang.h
@@ -1,4 +1,16 @@
-void func1(int n, char ch);
+#define OK 0
+#define SIZE_ERROR -1
+#define CHAR_TO_PRINT_ERROR -2
+/*
+  discription - *
+                **
+                ***
+  input - 
+  output - OK or ERROR
+  error - SIZE_ERROR or CHAR_TO_PRINT_ERROR
+*/
+
+void Print_left_aligned_triangle(int n, char ch);
 void func2(int n, char ch);
 void func3(int n, char ch);
 void func4(int n, char ch);
